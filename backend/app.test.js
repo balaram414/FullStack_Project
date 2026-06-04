9e9e96afa2cbec8951a98ce204bfab2529e588c2
Balaram <balarammanadal2404@gmail.com>
2026-06-04 09:04:47 +0530

    added unit test

---
+++ backend/app.test.js
@@ -0,0 +1,10 @@
+import request from "supertest";
+import app from "./index";
+
+describe("GET /", () => {
+  test("should return API Working", async () => {
+    const res = await request(app).get("/");
+
+    expect(res.statusCode).toBe(200);
+  });
+});
