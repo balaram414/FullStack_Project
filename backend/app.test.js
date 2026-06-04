9a3904bd1c30550bb7a734e9b570e75267aade7f
Balaram <balarammanadal2404@gmail.com>
2026-06-04 09:23:28 +0530

    testing for hook

--- backend/app.test.js
+++ backend/app.test.js
@@ -4,7 +4,6 @@ import app from "./index";
 describe("GET /", () => {
   test("should return API Working", async () => {
     const res = await request(app).get("/");
-
     expect(res.statusCode).toBe(200);
   });
 });
